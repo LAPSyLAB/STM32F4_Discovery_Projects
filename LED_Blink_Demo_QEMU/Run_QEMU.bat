ed1e070ef1f145afd3acdb32414c5c300583fb25
RR <rob.r374@gmail.com>
2022-05-19 16:43:31 +0200

    QEMU project additions...

--- LED_Blink_Demo_QEMU/Run_QEMU.bat
+++ LED_Blink_Demo_QEMU/Run_QEMU.bat
@@ -1,1 +1,1 @@
-.\xpack-qemu-arm-2.8.0-12\bin\qemu-system-gnuarmeclipse --verbose --verbose --board STM32F4-Discovery --mcu STM32F407VG --gdb tcp::1234 -d unimp,guest_errors  --semihosting-config enable=on,target=native --semihosting-cmdline LED_Blink_Demo
+.\xpack-qemu-arm-2.8.0-12\bin\qemu-system-gnuarmeclipse --verbose --verbose --board STM32F4-Discovery --mcu STM32F407VG --gdb tcp::1234 -d unimp,guest_errors  --semihosting-config enable=on,target=native --semihosting-cmdline LED_Blink_Demo_QEMU
